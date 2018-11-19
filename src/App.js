f6dc38187e2ecb99f5e8ff4ec12f04d57c2aa8aa
Fábio Santos <fabiosantos.pe@gmail.com>
2018-11-19 01:05:51 -0300

    v1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Game from './components/Game.jsx';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+// import './assets/bootstrap.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div style={{ flexGrow: 1 }}>
+        <AppBar position="static" color="primary" style={{ marginBottom: '40px' }}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Jogo da Velha (PWA)
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Game />
+      </div>
+    );
+  }
+}
